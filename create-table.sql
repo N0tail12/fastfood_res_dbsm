dd95b8091a8553c876744f3d5b10c88d6c0762ba
kuma <datlt132@gmail.com>
2021-01-17 03:22:04 +0700

    update table-insert

--- create-table.sql
+++ create-table.sql
@@ -1,50 +1,55 @@
-Create table customer_info(
-	email varchar(50)NOT NULL,
-	fname varchar(30),
-	lname varchar(15),
-	pass varchar(15),
-	phone varchar(15),
-	area varchar(30),
-	town varchar(30),
-	status varchar(10),
-	PRIMARY KEY(email)
+CREATE TABLE "customer_info" (
+  "email" varchar(20) PRIMARY KEY NOT NULL,
+  "fname" varchar(12),
+  "lname" varchar(12),
+  "pass" varchar(15),
+  "phone" varchar(14),
+  "area" varchar(10),
+  "town" varchar(10),
+  "status" varchar(10)
 );
 
-Create table order_info(
-	order_id varchar(5)NOT NULL,
-	email varchar(50),
-	TORCV Date,
-	TODEL Date,
-	PRIMARY KEY (order_id),
-	FOREIGN KEY (email) References customer_info (email)
+CREATE TABLE "order_info" (
+  "order_id" varchar(5) PRIMARY KEY NOT NULL,
+  "email" varchar(20),
+  "TORCV" Date,
+  "TODEL" Date
 );
 
-Create table management(
- 	user_id varchar(30)NOT NULL,
- 	pass varchar(15),
- 	user_name varchar(50),
- 	status varchar(10),
- 	designation varchar(15),
- 	PRIMARY KEY (user_id)
- );
-
-Create table menu(
-	item_id varchar(7)NOT NULL,
-	item_name varchar(30),
-	description varchar(30),
-	price money,
-	category varchar(20),
-	status varchar(10),
-	user_id varchar(30),
-	PRIMARY KEY (item_id),
-	FOREIGN KEY (user_id) References management(user_id)
+CREATE TABLE "management" (
+  "user_id" varchar(20) PRIMARY KEY NOT NULL,
+  "pass" varchar(15),
+  "user_name" varchar(15),
+  "status" varchar(5),
+  "designation" varchar(8)
 );
 
-Create table order_items(
- 	order_id varchar(5) NOT NULL,
- 	item_id varchar(7) NOT NULL,
- 	quantity int4,
- 	status varchar(15),
- 	user_id varchar(30),
- 	PRIMARY KEY ( order_id, item_id )
- );
+CREATE TABLE "menu" (
+  "item_id" varchar(7) PRIMARY KEY NOT NULL,
+  "item_name" varchar(15),
+  "description" varchar(30),
+  "price" money,
+  "catagory" varchar(8),
+  "status" varchar(10),
+  "user_id" varchar(20)
+);
+
+CREATE TABLE "order_items" (
+  "order_id" varchar(5) NOT NULL,
+  "item_id" varchar(7) NOT NULL,
+  "quantity" int4,
+  "status" varchar(10),
+  "user_id" varchar(20),
+  PRIMARY KEY ("order_id", "item_id")
+);
+
+ALTER TABLE "order_info" ADD FOREIGN KEY ("email") REFERENCES "customer_info" ("email");
+
+ALTER TABLE "menu" ADD FOREIGN KEY ("user_id") REFERENCES "management" ("user_id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("order_id") REFERENCES "order_info" ("order_id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("user_id") REFERENCES "management" ("user_id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("item_id") REFERENCES "menu" ("item_id");
+
