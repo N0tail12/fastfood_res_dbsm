893a3cbcd1777d93647cba167391e253101b7702
bluemoon132 <65409701+bluemoon132@users.noreply.github.com>
2021-01-16 12:14:04 +0700

    Sửa các menu thành các hàm (cho gọn)

--- main.py
+++ main.py
@@ -89,162 +89,6 @@ def check_account(conn, username, userpass):
     return check
 
 
-def is_day(string):
-    format = "%Y-%m-%d"
-    i = 1
-    try:
-        datetime.datetime.strptime(string, format)
-    except ValueError:
-        i = 0
-        print("This is the incorrect date string format. It should be YYYY-MM-DD")
-    return i
-
-
-def sale_by_day(conn):
-    i = 0
-    while i == 0:
-        day_start = input("Choose day start: (YYYY-MM-DD) ")
-        i = is_day(day_start)
-    i = 0
-    while i == 0:
-        day_end = input("Choose day end: (YYYY-MM-DD) ")
-        i = is_day(day_end)
-
-    sale = query(conn, f"select oi.todel, sum(me.price * ot.quantity) as total\
-                        from menu as me, order_items  as ot, order_info as oi\
-                        where me.item_id = ot.item_id and ot.order_id = oi.order_id\
-                            and todel between '{day_start}' and '{day_end}' and ot.status != 'Cancel'\
-                        group by(todel)\
-                        order by(total) desc")
-    for items in sale:
-        print(tabulate(items, headers=['Date', 'Sale']))
-    averge = query(conn, f"with bill_month as (\
-                            select oi.todel, sum(me.price * ot.quantity)as total\
-                            from menu as me, order_items  as ot, order_info as oi\
-                            where me.item_id = ot.item_id and ot.order_id = oi.order_id\
-                                and todel between '{day_start}' and '{day_end}' and ot.status != 'Cancel'\
-                            group by(todel)\
-                            )\
-                        select sum(total)/count(*) AS averge\
-                        from bill_month;")
-    for items in averge:
-        for item in items:
-            print("------------------")
-            print(f"Total: {item[0]}")
-
-
-def login(conn):
-    username = input("Enter your email or username: ")
-    userpass = input("Enter your password: ")
-    i = check_account(conn, username, userpass)
-    select_1 = select_2 = select_3 = 9,
-    count = 0
-    while i == 0 and count < 3:
-        print("Please enter again!")
-        username = input("Enter your email: ")
-        userpass = input("Enter your password: ")
-        i = check_account(conn, username, userpass)
-        count += 1
-    if i == -1:
-        print("Goodbye!")
-        return
-    if count == 3:
-        char = input("Do you want to create a new account! (y/n)")
-        while char != 'y' and char != 'n':
-            char = input("Do you want to create a new account! (y/n)")
-        if char == 'n':
-            print("Goodbye!")
-            return
-        else:
-            check = 0
-            while check != 1:
-                email = input("Enter your email:")
-                if '@' not in email:
-                    email = input("Your email is wrong! Enter again:")
-                fname = input("Enter your firstname:")
-                lname = input("Enter your lastname:")
-                password = input("Enter your password:")
-                phone = input("Enter your phone number:")
-                if len(phone) == 11:
-                    phone= input("Your phone number is wrong! Please enter again:")
-                city = input("Enter your city:")
-                town = input("Enter your town:")
-                check = insert(conn, f"INSERT INTO customer_info VALUES ('{email}','{fname}','{lname}','{password}','{phone}','{city}','{town}','Active');")
-    elif i == 1:
-        while select_1 != 5:
-            print("-----------CUSTOMER MENU:----------\n")
-            print("1. Customer information")
-            print("2. Show your previous order")
-            print("3. Order")
-            print("4. Check your present order")
-            print("5. Exit\n")
-            select_1 = int(input("Enter your choice: "))
-            if select_1 == 1 : show_info_customer(conn, username)
-            elif select_1 == 2 : show_old_order(conn, username)
-            elif select_1 == 3 : order(conn, username)
-            elif select_1 == 4 : check_present_order(conn, username)
-            while select_1 < 1 or select_1 > 5:
-                print("Invalid selection!")
-                select_1 = input("Enter your choice: ")
-    elif i == 2:
-        while select_2 != 5:
-            print("-----------EMPLOYEE MENU----------\n")
-            print("1. Employee Information")
-            print("2. Check order")
-            print('3. Complete order')
-            print("4. Show order can be delivery today")
-            print("5. Exit\n")
-            select_2 = int(input("Enter your choice: "))
-            while select_2 < 1 or select_2 > 6:
-                print("Invalid selection!")
-                select_2 = input("Enter your choice: ")
-            if select_2 == 1:
-                show_employee_info(conn, username)
-            if select_2 == 2:
-                show_order(conn)
-            if select_2 == 3:
-                complete_order(conn)
-            if select_2 == 4:
-                check_order(conn)
-    elif i == 3:
-        while select_3 != 5:
-            print("-----------MANAGER MENU----------\n")
-            print("1. Database Management")
-            print("2. Sales Management")
-            print("3. Overview of sales areas")
-            print("4. Exit\n")
-            select_3 = int(input("Enter your choice: "))
-            while select_3 < 1 or select_3 > 4:
-                print("Invalid selection!")
-                select_3 = input("Enter your choice: ")
-            if select_3 == 1:
-                print("-----------Database Management----------\n")
-                print("1. Customer Information Management")
-                print("2. Personnel Management")
-                print("3. Show Order Information")
-                print("4. Edit Menu")
-                print("5. Exit\n")
-                select_3_ = int(input("Enter your choice: "))
-                while select_3_ < 1 or select_3_ > 5:
-                    print("Invalid selection!")
-                    select_3_ = input("Enter your choice: ")
-                if select_3_ == 1:
-                    search_by_field(conn)
-            if select_3 == 2:
-                print("-----------Sales Management----------\n")
-                sale_by_day(conn)
-            if select_3 == 3:
-                print("-----------Overview of sales areas----------\n")
-                print("1. Most cancel area")
-                print("2. Number of dishes sold in each area")
-                print("3. Top 3 towns have the most customers")
-                print("4. Exit\n")
-                select_3_ = int(input("Enter your choice: "))
-                while select_3_ < 1 or select_3_ > 5:
-                    print("Invalid selection!")
-                    select_3_ = input("Enter your choice: ")
-
-
 def search_by_field(conn):
     lst = query(conn, f"select email, fname, lname, pass, phone, area, town, status\
                         from customer_info\
@@ -268,7 +112,7 @@ def search_by_field(conn):
                 else:
                     print("Your search: ")
                     print(tabulate(lst_item, headers=['email', 'fname', 'lname', 'pass', 'phone', 'area', 'town', 'status']))
-                r = input("Do you want to search another? Enter EXIT to exit\n")
+                r = input("\nDo you want to search another? Enter EXIT to exit\n")
                 if r != 'EXIT':
                     select = ''
                     continue
@@ -461,11 +305,289 @@ def check_present_order(conn, username):
         return
 
 
+def is_day(string):
+    format = "%Y-%m-%d"
+    i = 1
+    try:
+        datetime.datetime.strptime(string, format)
+    except ValueError:
+        i = 0
+        print("This is the incorrect date string format. It should be YYYY-MM-DD")
+    return i
+
+
+def sale_by_day(conn):
+    i = 0
+    while i == 0:
+        day_start = input("Choose day start: (YYYY-MM-DD) ")
+        i = is_day(day_start)
+    i = 0
+    while i == 0:
+        day_end = input("Choose day end: (YYYY-MM-DD) ")
+        i = is_day(day_end)
+
+    sale = query(conn, f"select oi.todel, sum(me.price * ot.quantity) as total\
+                        from menu as me, order_items  as ot, order_info as oi\
+                        where me.item_id = ot.item_id and ot.order_id = oi.order_id\
+                            and todel between '{day_start}' and '{day_end}' and ot.status != 'Cancel'\
+                        group by(todel)\
+                        order by(total) desc")
+    for items in sale:
+        print(tabulate(items, headers=['Date', 'Sale']))
+    averge = query(conn, f"with bill_month as (\
+                            select oi.todel, sum(me.price * ot.quantity)as total\
+                            from menu as me, order_items  as ot, order_info as oi\
+                            where me.item_id = ot.item_id and ot.order_id = oi.order_id\
+                                and todel between '{day_start}' and '{day_end}' and ot.status != 'Cancel'\
+                            group by(todel)\
+                            )\
+                        select sum(total)/count(*) AS averge\
+                        from bill_month;")
+    for items in averge:
+        for item in items:
+            print("------------------")
+            print(f"Total: {item[0]}")
+    r = input("Press ENTER to continue!")
+
+
+def most_cancel_area(conn):
+    lst = query(conn, f"select town, count(*) as num_cancel\
+                        from order_info oi, order_items ot, customer_info c\
+                        where oi.order_id = ot.order_id and ot.status = 'Cancel' and oi.email = c.email\
+                        group by(town)\
+                        order by(num_cancel) desc\
+                        limit 3")
+    for items in lst:
+        print(tabulate(items, headers=['Town', 'Number cancel times']))
+    r = input("Press ENTER to continue!")
+
+
+def num_dish_sold(conn):
+    lst = query(conn, f"select town, item_name, sum(quantity) as total_num\
+                        from menu m, customer_info c, order_items ot, order_info oi\
+                        where c.email = oi.email and ot.order_id = oi.order_id \
+                            and ot.item_id = m.item_id and ot.status != 'Cancel'\
+                        group by(town, item_name)\
+                        order by(town, item_name)")
+    for items in lst:
+        print(tabulate(items, headers=['Town', 'Name', 'Total']))
+    r = input("Press ENTER to continue!")
+
+
+def town_have_most_cus(conn):
+    lst = query(conn, f"select town, count(email)\
+                        from customer_info \
+                        group by town\
+                        order by count(email) desc\
+                        limit 3")
+    for items in lst:
+        print(tabulate(items, headers=['Town', 'Total customer']))
+    r = input("Press ENTER to continue!")
+
+
+def the_most_order_dish(conn):
+    lst = query(conn, f"select menu.item_id,menu.item_name , sum(quantity) as total_quantity\
+                        from order_items, menu\
+                        where order_items.item_id = menu.item_id\
+                        group by(menu.item_id)\
+                        order by(total_quantity) desc")
+    for items in lst:
+        print(tabulate(items, headers=['ID', 'Name', 'Total']))
+    r = input("Press ENTER to continue!")
+
+
+def show_order_by_field(conn):
+    lst = query(conn, f"select ot.order_id, item_name, quantity, email, ot.status, ot.user_id\
+                        from order_items ot, order_info oi, menu m\
+                        where ot.order_id = oi.order_id and ot.item_id = m.item_id\
+                        limit 1")
+    for items in lst:
+        print(tabulate(items, headers=['ot.order_id', 'item_name', 'quantity', 'email', 'ot.status', 'ot.user_id']))
+    head = ['ot.order_id', 'item_name', 'quantity', 'email', 'ot.status', 'ot.user_id']
+    select = ''
+    while select not in head:
+        select = input("Choose a field to search: ")
+        if select in head:
+            keyword = input("Input keyword: ")
+            _lst = query(conn, f"   select ot.order_id, item_name, quantity, email, ot.status, ot.user_id\
+                                    from order_items ot, order_info oi, menu m\
+                                    where ot.order_id = oi.order_id and ot.item_id = m.item_id\
+                                        and {select} = '{keyword}'")
+            for lst_item in _lst:
+                if not lst_item:
+                    print("No record macth your search!")
+                else:
+                    print("Your search: ")
+                    print(tabulate(lst_item,
+                                   headers=['ot.order_id', 'item_name', 'quantity', 'email', 'ot.status', 'ot.user_id']))
+                r = input("\nDo you want to search another? Enter EXIT to exit\n")
+                if r != 'EXIT':
+                    select = ''
+                    continue
+        else:
+            print("Your field is invalid!")
+            print("Choose another field to search: ")
+
+
+def show_manager_menu():
+    print("-----------MANAGER MENU----------\n")
+    print("1. Database Management")
+    print("2. Sales Management")
+    print("3. Overview of sales areas")
+    print("4. Exit\n")
+
+
+def show_database_manager_menu():
+    print("-----------Database Management----------\n")
+    print("1. Customer Information Management")
+    print("2. Personnel Management")
+    print("3. Show Order Information")
+    print("4. Edit Menu")
+    print("5. Exit\n")
+
+
+def show_orderview_area_menu():
+    print("-----------Overview of sales areas----------\n")
+    print("1. Most cancel area")
+    print("2. Number of dishes sold in each area")
+    print("3. Top 3 towns have the most customers")
+    print("4. Exit\n")
+
+
+def login(conn):
+    username = input("Enter your email or username: ")
+    userpass = input("Enter your password: ")
+    i = check_account(conn, username, userpass)
+    select_1 = select_2 = select_3 = 9,
+    count = 0
+    while i == 0 and count < 3:
+        print("Please enter again!")
+        username = input("Enter your email: ")
+        userpass = input("Enter your password: ")
+        i = check_account(conn, username, userpass)
+        count += 1
+    if i == -1:
+        print("Goodbye!")
+        return
+    if count == 3:
+        char = input("Do you want to create a new account! (y/n)")
+        while char != 'y' and char != 'n':
+            char = input("Do you want to create a new account! (y/n)")
+        if char == 'n':
+            print("Goodbye!")
+            return
+        else:
+            check = 0
+            while check != 1:
+                email = input("Enter your email:")
+                if '@' not in email:
+                    email = input("Your email is wrong! Enter again:")
+                fname = input("Enter your firstname:")
+                lname = input("Enter your lastname:")
+                password = input("Enter your password:")
+                phone = input("Enter your phone number:")
+                if len(phone) == 11:
+                    phone= input("Your phone number is wrong! Please enter again:")
+                city = input("Enter your city:")
+                town = input("Enter your town:")
+                check = insert(conn, f"INSERT INTO customer_info VALUES ('{email}','{fname}','{lname}','{password}','{phone}','{city}','{town}','Active');")
+    elif i == 1:
+        while select_1 != 5:
+            print("-----------CUSTOMER MENU:----------\n")
+            print("1. Customer information")
+            print("2. Show your previous order")
+            print("3. Order")
+            print("4. Check your present order")
+            print("5. Exit\n")
+            select_1 = int(input("Enter your choice: "))
+            if select_1 == 1 : show_info_customer(conn, username)
+            elif select_1 == 2 : show_old_order(conn, username)
+            elif select_1 == 3 : order(conn, username)
+            elif select_1 == 4 : check_present_order(conn, username)
+            while select_1 < 1 or select_1 > 5:
+                print("Invalid selection!")
+                select_1 = input("Enter your choice: ")
+    elif i == 2:
+        while select_2 != 5:
+            print("-----------EMPLOYEE MENU----------\n")
+            print("1. Employee Information")
+            print("2. Check order")
+            print('3. Complete order')
+            print("4. Show order can be delivery today")
+            print("5. Exit\n")
+            select_2 = int(input("Enter your choice: "))
+            while select_2 < 1 or select_2 > 6:
+                print("Invalid selection!")
+                select_2 = input("Enter your choice: ")
+            if select_2 == 1:
+                show_employee_info(conn, username)
+            if select_2 == 2:
+                show_order(conn)
+            if select_2 == 3:
+                complete_order(conn)
+            if select_2 == 4:
+                check_order(conn)
+    elif i == 3:
+        while select_3 != 5:
+            show_manager_menu()
+            select_3 = int(input("Enter your choice: "))
+            while select_3 < 1 or select_3 > 4:
+                print("Invalid selection!")
+                select_3 = input("Enter your choice: ")
+            if select_3 == 1:
+                show_database_manager_menu()
+                select_3_ = int(input("Enter your choice: "))
+                while select_3_ < 1 or select_3_ > 5:
+                    print("Invalid selection!")
+                    select_3_ = input("Enter your choice: ")
+                if select_3_ == 1:
+                    search_by_field(conn)
+                elif select_3_ == 2:
+                    pass
+                elif select_3_ == 3:
+                    while luachon != 3:
+                        print("-----------Order Management----------\n")
+                        print("1. Show all order")
+                        print("2. The most order dish")
+                        print("3. Exit")
+                        luachon = int(input("Enter your choice: "))
+                        while luachon < 1 or luachon > 5:
+                            print("Invalid selection!")
+                            luachon = input("Enter your choice: ")
+                        if luachon == 1:
+                            show_order_by_field(conn)
+                        elif luachon == 2:
+                            the_most_order_dish(conn)
+                elif select_3_ == 4:
+                    pass
+            elif select_3 == 2:
+                print("-----------Sales Management----------\n")
+                sale_by_day(conn)
+            elif select_3 == 3:
+                while select_3_ != 4:
+                    show_orderview_area_menu()
+                    select_3_ = int(input("Enter your choice: "))
+                    while select_3_ < 1 or select_3_ > 5:
+                        print("Invalid selection!")
+                        select_3_ = input("Enter your choice: ")
+                    if select_3_ == 1:
+                        most_cancel_area(conn)
+                    elif select_3_ == 2:
+                        num_dish_sold(conn)
+                    elif select_3_ == 3:
+                        town_have_most_cus(conn)
+
+
 def main():
     conn = connect_to_db('res', 'postgres', 'admin', 'localhost')
     # login(conn)
     # search_by_field(conn)
-    sale_by_day(conn)
+    # sale_by_day(conn)
+    # most_cancel_area(conn)
+    # num_dish_sold(conn)
+    # town_have_most_cus(conn)
+    # the_most_order_dish(conn)
+    show_order_by_field(conn)
     disconnect_to_db(conn)
 
 
