fe6b1c7337d151801aedfb63f1d0b81184f08687
N0tail12 <62517449+N0tail12@users.noreply.github.com>
2021-04-26 23:19:22 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 app.use(express.static("public"));
 app.use('/css', express.static(__dirname + '/lib/bootstrap/css'));
@@ -7,19 +8,46 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 app.listen(3000);
 
+app.use(express.urlencoded({extended: false}));
+
 const {Pool} = require('pg')
 const connectionString = 'postgres://gecksmtj:8xTHFHDY7Nqu80PT8yv_0OLZi7sA1Uz9@suleiman.db.elephantsql.com:5432/gecksmtj'
 const pool = new Pool({
   connectionString,
 })
 
-app.get('/', function(req, res){
-    res.render("main.ejs");
-})
 
-app.get('/login', function(req, res){
-    res.render("login.ejs");
-})
+app.get('/', function(req, res){
+  res.render("main");
+});
+
+
+app.post('/info', async (req, res) =>{
+  var user = req.body.txtemail;
+  var pass = req.body.pass;
+  // console.log(user);
+  // console.log(pass);
+  try{
+    let rs = await pool.query("Select * from customer_info where email = '"+user+"' and pass = '"+pass+"' and status = 'Active'");
+    if(rs.rowCount > 0){
+      res.render('customer');
+    }
+    else{
+      rs = await pool.query("Select * from customer_info where email = '"+user+"' and pass = '"+pass+"'");
+    }
+  }catch(err){
+
+  }
+});
+
+
+
+
+
+
+// app.get('/login', function(req, res){
+//     res.render("login.ejs");
+// })
 
 
 
