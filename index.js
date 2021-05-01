8202b6b1b054f98ccd225450ddab50a411d606d6
N0tail12 <62517449+N0tail12@users.noreply.github.com>
2021-05-02 00:38:43 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 app.use(express.static("public"));
 app.use('/css', express.static(__dirname + '/lib/bootstrap/css'));
 app.use('/JOEY2.png', express.static(__dirname + '/views/JOEY2.png'))
+app.use('/style.css', express.static(__dirname + '/public/style.css'));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.listen(3000);
@@ -21,6 +22,22 @@ app.get('/', function(req, res){
   res.render("main");
 });
 
+app.post('/', async (req,res)=>{
+  let fname = req.body.signfname;
+  let lnane = req.body.signlname;
+  let email = req.body.signemail;
+  let pass = req.body.signpass;
+  let pnumber = req.body.signpnumber;
+  let area = req.body.signarea;
+  let town = req.body.signtown;
+  try{
+    let rs = await pool.query("INSERT INTO customer_info VALUES ('"+email+"','"+fname+"','"+lnane+"','"+pass+"','"+pnumber+"','"+area+"','"+town+"','Active');")
+    console.log("Added");
+    res.render("main");
+  }catch(err){
+    console.log("Smthing went wrong!");
+  }
+});
 
 app.post('/login', async (req, res) =>{
   var user = req.body.txtemail;
@@ -30,21 +47,19 @@ app.post('/login', async (req, res) =>{
   try{
     let rs = await pool.query("Select * from customer_info where email = '"+user+"' and pass = '"+pass+"' and status = 'Active'");
     if(rs.rowCount > 0){
-       console.log(user);
-       console.log(pass);
-       res.render('login');
+       res.render('customer');
     }
     else{
       rs = await pool.query("Select * from management where user_id = '"+user+"' and pass = '"+pass+"' and status = 'Actv'");
        if(rs.rows[0].designation == 'Manager'){
-         res.render('login');
+         res.render('manager');
        }
        else if(rs.rows[0].designation == 'Employee' || rs.rows[0].designation == 'Cook'){
-        res.render('customer')
+        res.render('employee')
        }
     }
   }catch(err){
-
+    console.log("Smthing went wrong!");
   }
 });
 
