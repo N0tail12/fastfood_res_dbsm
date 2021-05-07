68615588de2e9dbd2e6920ceba2d6224e8394eaa
N0tail12 <62517449+N0tail12@users.noreply.github.com>
2021-05-07 23:45:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var items
 app.use(express.static("public"));
 app.use('/css', express.static(__dirname + '/lib/bootstrap/css'));
+app.use('/js', express.static(__dirname + '/lib/bootstrap/js'));
 // app.use('/JOEY2.png', express.static(__dirname + '/views/JOEY2.png'));
 // app.use('/hamburger.jpg', express.static(__dirname + '/views/hamburger.jpg'));
 // app.use('/pizza.jpg', express.static(__dirname + '/views/pizza.jpg'));
@@ -23,19 +24,37 @@ app.listen(3000);
 app.use(express.urlencoded({ extended: false }));
 app.use(flash());
 app.use(cookieParser('secretString'));
-app.use(session({ cookie: { maxAge: 60000 } }));
+app.use(session({ cookie: { maxAge: 60 } }));
 
 const { Pool } = require('pg');
 const connectionString = 'postgres://ijgbgbrz:UXX0MoGyaIw8cXPmUdHI5aWas3HHP5Oy@queenie.db.elephantsql.com:5432/ijgbgbrz'
 const pool = new Pool({
   connectionString,
 })
+app.get('/manager',(req,res)=>{
+  res.render('manager')
+})
 
-app.get('/manager', async (req, res) => {
+app.get('/manager/customerinfo', async (req, res) => {
+  let rs = await pool.query("Select * from customer_info");
+  let page = req.query.page;
+  let limit = 60
+  if(page < 1)
+  page = 1;
+  if(page > (rs.rowCount/limit))
+  page = Math.ceil(rs.rowCount/limit);
+  const startIndex = (page - 1) * limit;
+  const endIndex = page*limit;
+  const info = rs.rows.slice(startIndex, endIndex);
+  // console.log(rs.rows)
   // let rs = await pool.query('select * from menu');
   // items = res.json(rs.rows);  
-  res.render('manager')
+  res.render('customerinfo', {info: info, page: page, numberpage: rs.rowCount/limit})
 });
+app.get('/customer', (req,res)=>{
+
+  res.render('customer')
+})
 // app.get('/manager',async (req,res)=>{
 //   let rs = await pool.query('select * from menu')
 //   items = res.json(rs.rows)
@@ -75,18 +94,17 @@ app.post('/home', async (req, res) => {
   // console.log(user);
   // console.log(pass);
   try {
-    let rs = await pool.query("Select * from customer_info where email = '" + user + "' and pass = '" + pass + "' and status = 'Active'");
+    let rs = await pool.query("Select * from customer_info where email = '" + user + "' and pass = '" + pass + "' and status = 'Active'")
     if (rs.rowCount > 0) {
-      req.flash('message', 'Login Success')
-      req.flash('type', 'success')
-      res.render('customer', { message: req.flash('message'), type: req.flash('type') });
-      items = res.json(rs.rows)
+      res.cookie('user', 'cookie')
+      res.redirect('/customer?user=' + user + '&page=1')
+      // items = res.json(rs.rows)
       //res.render('customer',message);
     }
     else {
       rs = await pool.query("Select * from management where user_id = '" + user + "' and pass = '" + pass + "' and status = 'Actv'");
       if (rs.rows[0].designation == 'Manager') {
-        res.redirect('/manager');
+        res.redirect('/manager?user='+ user);
       }
       else if (rs.rows[0].designation == 'Employee' || rs.rows[0].designation == 'Cook') {
         req.flash('message', 'Login Success')
@@ -100,7 +118,6 @@ app.post('/home', async (req, res) => {
     res.render('main', { message: req.flash('message'), type: req.flash('type') });
   }
 });
-
 
 
 // app.get('/login', function(req, res){
